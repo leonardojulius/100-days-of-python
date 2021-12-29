e2366cc19f76a55c1ad03b1bde089e220dd8fe57
leonardojulius <leonardojulius.jcl@gmail.com>
2021-12-29 20:02:37 +0800

    Update day-4-2.py
    Instructions
    You are going to write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
    
    Important: You are not allowed to use the choice() function.
    
    Line 8 splits the string names_string into individual names and puts them inside a List called names. For this to work, you must enter all the names as names followed by comma then space. e.g. name, name, name
    
    When you run the code, just use a random number as the seed. e.g. 67346 It doesn't matter what you chose, it's only for our testing code to check your work.
    
    Example Input
    Angela, Ben, Jenny, Michael, Chloe
    Note: notice that there is a space between the comma and the next name.
    
    Example Output
    Michael is going to buy the meal today!
    Hint
    You might need the help of the len() function.
    https://stackoverflow.com/questions/1712227/how-do-i-get-the-number-of-elements-in-a-list
    
    Remember that Lists start at index 0!
    Test Your Code
    Check your code is doing what it is supposed to. When you're happy with your code, click submit to check your solution.
    
    Solution
    https://repl.it/@appbrewery/day-4-2-solution

--- day-4-2.py
+++ day-4-2.py
@@ -1,16 +1,8 @@
 import random
 
-# Split string method
-names_string = input("Give me everybody's names, seperated by a comma. ")
-names = names_string.split(", ")
+#seed_number=input("Create a seed number:")
+#random.seed(seed_number)
 
-#Write your code below this line 👇
+names_string = input("Give me everybody's names, separated by a comma. ").split(", ")
 
-#Get the total number of items in list.
-num_items = len(names)
-#Generate random numbers between 0 and the last index. 
-random_choice = random.randint(0, num_items - 1)
-#Pick out random person from list of names using the random number.
-person_who_will_pay = names[random_choice]
-
-print(person_who_will_pay + " is going to buy the meal today!")
+print(f"{names_string [random.randint(0,len(names_string)-1)]} is going to buy the meal today!")
